f6232c0e914d2173b87e0f3397a2d2029ad350b1
Adam Presley <adam@adampresley.com>
2012-05-01 14:20:47 -0500

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-sublime-js-minify
-=================
+# Sublime JavaScript Minifier
+
+## About
+Sublime JavaScript Minifier is a Sublime Text 2 plugin designed to allow
+users an easy way to minify a JavaScript file they are working on. It also sports
+the ability to try and find an existing minified file and alter it based on a 
+configurable list of file name suffixes. For example, when minifying *yourFile.js*
+it will look for *youFile.min.js* or *yourFile-min.js*.
+
+
+## Installation
+To "install" this plugin find your User Packages directory for Sublime. In Windows 7
+this would be something to the effect of **C:\Users\me\AppData\Roaming\Sublime Text 2\Packages\User**.
+For most *nix distributions that would be **/home/me/.conf/Sublime Text 2/Packages/User**.
+
+Download the project and place the *sublime-js-minify* directory into the above packages directory. 
+This will load up the plugin. It also will create menus, context menus, and a key binding. 
+
+This plugin, however, depends on the Yahoo! YUI Compressor, which is provided along with this
+repository. Take the *yuicompressor-2.4.7* directory and place it somewhere. Anywhere you
+can get to it is fine. Once this is done you will need to alter the following files for
+the minification tool to work:
+
+* Main.sublime-menu
+* Default (Windows).sublime-keymap
+* Default (OSX).sublime-keymap
+* Default (Linux).sublime-keymap
+* Context.sublime-menu
+
+Note that you don't have to modify every key map file, just the one that applies to the OS
+you are using. In each of those files you will see a key labelled **"command"** that has
+a path to the **yuicompressor-2.4.7.jar** file. Change the path to this to match where you
+placed the *yuicompressor-2.4.7* directory.
+
+Once complete you should be able to open a JavaScript file and execute it one of the 
+following ways:
+
+* CTRL + ALT + M
+* Right-click -> Adam Presley -> Minify JavaScript
+* Tools -> Adam Presley -> Minify JavaScript
+
+
+## License
 
 Sublime JavaScript Minifier
+Copyright (C) 2011 Adam Presley
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+adam [at] adampresley [dot] com
---
+++ sublime-js-minify/MinifyjsCommand.py
@@ -0,0 +1,133 @@
+#
+# History:
+# 		2/13/2012:
+# 			- Fixed issue with running on a file with a UNC path in Windows. Now uses the
+# 			  user's home directory as the current working directory.
+#
+import sublime, sublime_plugin
+import re, os, subprocess
+import time
+
+from subprocess import Popen
+
+targetView = None
+targetCode = None
+
+
+def replaceWithNewCode(view):
+	global targetCode
+
+	r = sublime.Region(0, view.size())
+
+	ed = view.begin_edit()
+	view.erase(ed, r)
+	s = view.insert(ed, 0, targetCode)
+	view.end_edit(ed)
+
+
+class LoadListener(sublime_plugin.EventListener):
+	def on_load(self, view):
+		global targetView
+
+		if view.file_name() == targetView:
+			replaceWithNewCode(view)
+			targetView = None
+
+
+class MinifyjsCommand(sublime_plugin.TextCommand):
+	__window = None
+	__view = None
+
+	def run(self, edit, command, args, suffixes):
+		#
+		# Assemble the command to the YUI compressor
+		#
+		currentBuffer = self.__getCurrentBufferInfo()
+		filename = currentBuffer["fileName"]
+		processCommand = self.__getCommand(command, args, filename)
+
+		self.__view = self.view
+		self.__window = self.view.window()
+
+		results = []
+
+
+		#
+		# Run it, compress it, capture it.
+		#
+		p = Popen(processCommand, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, cwd = os.getenv('USERPROFILE') or os.getenv('HOME'))
+		for line in p.stdout.readlines():
+			results.append(line)
+
+		ret = p.wait()
+
+		newCode = self.__getNewCode(results)
+
+
+		#
+		# Display the results in a new tab, or alter existing minified file.
+		#
+		if not self.__findExistingMinifiedMatches(filename, suffixes, newCode):
+			self.__displayResults(newCode)
+
+
+	def __getCurrentBufferInfo(self):
+		return { "id": self.view.buffer_id(), "fileName": self.view.file_name() }
+
+
+	def __getCommand(self, command, args, filename):
+		return "java -jar %s %s \"%s\"" % (command, args, filename)
+
+
+	def __getNewCode(self, output):
+		newCode = "".join(output)
+		return newCode
+
+
+	def __displayResults(self, code):
+		tab = self.__window.new_file()
+		ed = tab.begin_edit()
+		tab.insert(ed, 0, code)
+		tab.end_edit(ed)
+
+
+	def __findExistingMinifiedMatches(self, filename, suffixes, newCode):
+		global targetView
+		global targetCode
+
+		if filename == None:
+			return
+			
+		if not len(filename):
+			return False
+
+
+		namePart, extension = os.path.splitext(filename)
+		found = False
+		foundFileName = ""
+
+		tab = None
+
+		#
+		# Search for files that match the suffix settings passed in 
+		#
+		for suffix in suffixes:
+			fileToMatch = "%s%s%s" % (namePart, suffix, extension)
+
+			if os.path.isfile(fileToMatch):
+				found = True
+				foundFileName = fileToMatch
+				break
+
+		if found:
+			tab = self.__window.open_file(foundFileName)
+			targetCode = newCode
+
+			if not tab.is_loading():
+				replaceWithNewCode(tab)
+			else:
+				targetView = tab.file_name()
+
+			return True
+
+		return False
